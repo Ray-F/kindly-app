632b2f13ee4b958bd6cc6815167c4cfe84843270
Brendan Zhou <brendanzhou.99@gmail.com>
2022-07-10 02:52:18 +1200

    Added animation

--- packages/client/src/pages/DashboardPage.tsx
+++ packages/client/src/pages/DashboardPage.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@mui/styles';
-import Topbar from '../components/Topbar';
-import GestureCard from '../components/GestureCard';
-import styled from 'styled-components';
-import LeaderboardCard from '../components/LeaderboardCard';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@mui/styles";
+import Topbar from "../components/Topbar";
+import GestureCard from "../components/GestureCard";
+import styled from "styled-components";
+import LeaderboardCard from "../components/LeaderboardCard";
 
 const useStyles: Function = makeStyles(() => ({
   bodyMain: {
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: '#FFFFFF !important',
-    position: 'relative',
-    overflow: 'hidden',
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "#FFFFFF !important",
+    position: "relative",
+    overflow: "hidden",
   },
 
   cards: {
-    position: 'relative',
+    position: "relative",
     marginLeft: 70,
   },
 }));
@@ -49,7 +49,7 @@ const Container = styled.div`
     border-radius: 50%;
     box-shadow: 0 20px 20px 0 #aaa;
     left: 9.75%;
-    top: 30%;
+    top: 25%;
   }
 
   .circle-bead-on-line2 {
@@ -60,7 +60,7 @@ const Container = styled.div`
     border-radius: 50%;
     box-shadow: 0 20px 20px 0 #aaa;
     left: 9.75%;
-    top: 57.5%;
+    top: 50%;
   }
 
   .circle-bead-on-line3 {
@@ -71,28 +71,71 @@ const Container = styled.div`
     border-radius: 50%;
     box-shadow: 0 20px 20px 0 #aaa;
     left: 9.75%;
-    top: 85%;
+    top: 75%;
   }
 
   .leaderboard-section {
   }
 `;
 
-export default function DashboardPage() {
+interface PositionData {
+  thirdPos: number;
+  thirdPosX: number;
+  thirdSize: number;
+  thirdPoints: number;
 
+  twoPos: number;
+  twoPosX: number;
+  twoSize: number;
+  twoPoints: number;
+}
+
+export default function DashboardPage() {
   const [isLoading, setIsLoading] = useState(true);
 
   const [data, setData] = useState<any>({});
 
+  const [positionData, setPositionData] = useState<PositionData>({
+    thirdPos: 37.5,
+    thirdPosX: -17,
+    thirdSize: 140,
+    thirdPoints: 0,
+
+    twoPoints: 400,
+    twoPos: 60,
+    twoPosX: -12,
+    twoSize: 200,
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      switchPos(100);
+    }, 3000);
+  });
+
+  const switchPos = (thirdPointAdd: number) => {
+    setPositionData({
+      thirdPos: 60,
+      thirdPosX: -38,
+      thirdSize: 200,
+      thirdPoints: thirdPointAdd,
+
+      twoPoints: 450,
+      twoPos: 37.5,
+      twoPosX: 10,
+      twoSize: 140,
+    });
+  };
+
   useEffect(() => {
     console.log(process.env.REACT_APP_API_ENDPOINT);
     fetch(`${process.env.REACT_APP_API_ENDPOINT}/dashboard/rand-id`)
-        .then((it) => it.json())
-        .then((it) => {
-          setData(it);
-          setIsLoading(false);
-          console.log(it);
-        });
+      .then((it) => it.json())
+      .then((it) => {
+        setData(it);
+        setIsLoading(false);
+        console.log(it);
+      });
   }, []);
 
   const classes = useStyles();
@@ -101,37 +144,59 @@ export default function DashboardPage() {
     return <p>Is loading</p>;
   }
 
-
   return (
-      <div className={classes.bodyMain}>
-        <>
-          <Topbar />
-          <Container>
-            <div className="gesture-section">
-              <h1 style={{ fontSize: '45px' }}>Daily Gestures</h1>
-              <div className="harshals-hard-line"></div>
-              <div className="circle-bead-on-line"></div>
-              <div className="circle-bead-on-line2"></div>
-              <div className="circle-bead-on-line3"></div>
-              {data.gestures.map((it: any) => (
-                  <GestureCard img={it.iconUrl} content={it.name} points={it.points} completed={it.isCompleted} id={it.id} />
-              ))}
-            </div>
+    <div className={classes.bodyMain}>
+      <>
+        <Topbar />
+        <Container>
+          <div className="gesture-section">
+            <h1 style={{ fontSize: "45px" }}>Daily Gestures</h1>
+            <div className="harshals-hard-line"></div>
+            <div className="circle-bead-on-line"></div>
+            <div className="circle-bead-on-line2"></div>
+            <div className="circle-bead-on-line3"></div>
+            {data.gestures.map((it: any) => (
+              <GestureCard
+                img={it.iconUrl}
+                content={it.name}
+                points={it.points}
+                completed={it.isCompleted}
+                id={it.id}
+              />
+            ))}
+          </div>
 
-            <div className="leaderboard-section">
-              <h1 style={{ fontSize: '45px' }}>Leaderboard</h1>
-              <LeaderboardCard size={250} position={17.5} name={data.leaderboard[0].name}
-                               positionX={3}
-                               points={data.leaderboard[0].totalPoints} image={data.leaderboard[0].iconUrl} />
-              <LeaderboardCard size={200} position={60} name={data.leaderboard[1].name}
-                               positionX={-12}
-                               points={data.leaderboard[1].totalPoints} image={data.leaderboard[1].iconUrl} />
-              <LeaderboardCard size={140} position={37.5} name={data.leaderboard[2].name}
-                               positionX={-17}
-                               points={data.leaderboard[2].totalPoints} image={data.leaderboard[2].iconUrl} />
-            </div>
-          </Container>
-        </>
-      </div>
+          <div className="leaderboard-section">
+            <h1 style={{ fontSize: "45px" }}>Leaderboard</h1>
+            <LeaderboardCard
+              size={250}
+              position={17.5}
+              name={data.leaderboard[0].name}
+              positionX={3}
+              points={data.leaderboard[0].totalPoints}
+              image={data.leaderboard[0].iconUrl}
+            />
+            <LeaderboardCard
+              size={positionData.twoSize}
+              position={positionData.twoPos}
+              name={data.leaderboard[1].name}
+              positionX={positionData.twoPosX}
+              points={data.leaderboard[1].totalPoints}
+              image={data.leaderboard[1].iconUrl}
+            />
+            <LeaderboardCard
+              size={positionData.thirdSize}
+              position={positionData.thirdPos}
+              name={data.leaderboard[2].name}
+              positionX={positionData.thirdPosX}
+              points={
+                data.leaderboard[2].totalPoints + positionData.thirdPoints
+              }
+              image={data.leaderboard[2].iconUrl}
+            />
+          </div>
+        </Container>
+      </>
+    </div>
   );
 }
