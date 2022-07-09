31229b963618e8bf4accf2f9ec1df38bcd7c3eab
Raymond Feng <rf.raymondfeng@gmail.com>
2022-07-09 21:46:53 +1200

    Add slack front end

--- packages/client/src/components/Topbar.js
+++ packages/client/src/components/Topbar.js
@@ -1,67 +1,72 @@
-import React from 'react'
-import { makeStyles } from "@mui/styles";
-import user from "../resources/user.png";
-import kindly from "../resources/kindlylogo.png";
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import user from '../resources/profile_icon.png';
+import kindly from '../resources/kindlylogo.png';
 
 const useStyles = makeStyles(() => ({
-    bodyText: {
-        color: "#00B0BB",
-        textAlign: 'center',
-    },
-  
-    user: {
-        marginTop: 28,
-        marginRight: 25,
-    },
-  
-    kindly: {   
-        height: 55,
-        width: 55,
-        opacity: "55%",
-        marginTop: 15,
-        marginRight: 8,
-    },
+  bodyText: {
+    color: '#00B0BB',
+    textAlign: 'center',
+  },
 
-    containter: {
-        display: "flex",
-        flexFlow: "row nowrap",
-        width: "100vw",
-        height: "100wh",
-        justifyContent: "center'"
-    },
+  user: {
+    position: 'absolute',
+    height: 44,
+    marginTop: 16,
+    right: 50,
+  },
 
-    containerPT: {
-        display: "flex",
-        flexFlow: "row nowrap",
-        width: "100vw",
-        height: "100wh",
-        justifyContent: "center",
-    },
+  kindly: {
+    height: 40,
+    width: 'auto',
+    opacity: '55%',
+    marginTop: 15,
+    marginRight: 8,
+  },
 
-    topbar: {
-        borderBottom: "3px solid #F8F2F2"
-    }
-  }));
-  
+  containter: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    width: '100vw',
+    height: '100wh',
+    justifyContent: 'center\'',
+  },
+
+  containerPT: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    width: '100vw',
+    height: '100wh',
+    justifyContent: 'center',
+  },
+
+  topbar: {
+    position: 'absolute',
+    zIndex: 10,
+    borderBottom: '3px solid #F8F2F2',
+  },
+}));
 
 
 export default function Topbar() {
-    const classes = useStyles();
-    return(
-        <nav className={classes.topbar}>
-            <div className={classes.containter}>   
-                <div className={classes.containerPT}>
-                    <div>
-                        <img alt="" src={kindly} className={classes.kindly} />
-                    </div>
-                    <div className={classes.bodyText}>
-                        <h1 style={{ fontSize: "40px",}}>Kindly.</h1>
-                    </div>    
-                </div>
-                <div>
-                <img alt="" src={user} className={classes.user} />
-                </div>
-            </div>     
-        </nav>
-    )
+  const classes = useStyles();
+  return (
+    <nav className={classes.topbar}>
+      <div className={classes.containter}>
+        <div className={classes.containerPT}>
+          <div>
+            <img alt="" src={kindly} className={classes.kindly} />
+          </div>
+          <div className={classes.bodyText}>
+            <h1 style={{ fontSize: '30px' }}>Kindly.</h1>
+          </div>
+        </div>
+        <div>
+          <a href={'/profile'}>
+            <img alt="profile-menu" src={user} className={classes.user} />
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- packages/client/src/pages/OnboardingFinishSlack.tsx
+++ packages/client/src/pages/OnboardingFinishSlack.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import slackBtnMini from '../resources/onboarding/slack_button_mini.jpg';
+import slackBtnMini from '../resources/onboarding/slack_button_big.png';
 import bgImage from '../resources/onboarding/ba_graphic_for_export.png';
+import Topbar from '../components/Topbar';
 
 const Container = styled.div`
+  position: absolute;
+  top: 0;
+  
   width: 100vw;
   height: 100vh;
   display: flex;
-  
+
   justify-content: center;
   align-items: center;
-  
+
   text-align: center;
-  
-`
+
+`;
 
 const BGImage = styled.img`
   position: absolute;
@@ -21,33 +25,43 @@ const BGImage = styled.img`
   z-index: -1;
   width: 100vw;
   height: 100vh;
-  
+
   object-fit: cover;
   object-position: center;
-`
+`;
 
 const ConnectedDesc = styled.div`
   display: flex;
+  align-items: center;
   
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
   img {
     margin-left: 10px;
+    height: 44px;
     border-radius: 10px;
-    box-shadow: #efefef 0 10px 40px 0;
+    box-shadow: #efefef 0 5px 10px 0;
   }
-`
+`;
 
 function OnboardingFinishSlack() {
   return (
-      <Container>
-        <BGImage src={bgImage} alt={'background-img'} />
-        <div>
-          <h1>All set up!</h1>
-          <ConnectedDesc>
-            <p>connected with</p>
-            <img src={slackBtnMini}  alt={"slack button logo"}/>
-          </ConnectedDesc>
-        </div>
-      </Container>
+      <>
+        <Topbar />
+        <Container>
+          <BGImage src={bgImage} alt={'background-img'} />
+          <div>
+            <h1>All set up!</h1>
+            <ConnectedDesc>
+              <p>connected with</p>
+              <img src={slackBtnMini} alt={'slack button logo'} />
+            </ConnectedDesc>
+          </div>
+        </Container>
+      </>
   );
 }
 
